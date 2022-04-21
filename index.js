2c91ca4b4fc1be7393a685a8892b7c9f73fbd679
Andrew Faulkner <andfaulkner@gmail.com>
2022-04-21 10:47:35 -0400

    Upgraded to work with node.js 16+. Modernized formatting.
    Removed references to '.rli'. Ran prettier. Converted 'function' refs to arrow functions. Consolidated verbose code. Etc.

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
-var fs = require('fs');
-module.exports = function (repl, file) {
+const fs = require('fs');
 
-  try {
-    var stat = fs.statSync(file);
-    repl.rli.history = fs.readFileSync(file, 'utf-8').split('\n').reverse();
-    repl.rli.history.shift();
-    repl.rli.historyIndex = -1; // will be incremented before pop
-  } catch (e) {}
+module.exports = (repl, file) => {
+    try {
+        const stat = fs.statSync(file);
+        repl.history = fs.readFileSync(file, 'utf-8').split('\n').reverse();
+        repl.history.shift();
+        repl.historyIndex = -1; // will be incremented before pop
+    } catch (e) {}
 
-  var fd = fs.openSync(file, 'a'), reval = repl.eval;
-  var wstream = fs.createWriteStream(file, {
-    fd: fd
-  });
-  wstream.on('error', function(err) {
-    throw err;
-  });
+    const fd = fs.openSync(file, 'a');
+    const reval = repl.eval;
 
-  repl.rli.addListener('line', function(code) {
-    if (code && code !== '.history') {
-      wstream.write(code + '\n');
-    } else {
-      repl.rli.historyIndex++;
-      repl.rli.history.pop();
-    }
-  });
+    const wstream = fs.createWriteStream(file, {fd: fd});
+    wstream.on('error', err => {
+        throw err;
+    });
 
-  process.on('exit', function() {
-    fs.closeSync(fd);
-  });
+    repl.addListener('line', code => {
+        if (code && code !== '.history') {
+            wstream.write(code + '\n');
+        } else {
+            repl.historyIndex++;
+            repl.history.pop();
+        }
+    });
 
-  repl.commands['history'] = {
-    help : 'Show the history',
-    action : function() {
-      var out = [];
-      repl.rli.history.forEach(function(v, k) {
-        out.push(v);
-      });
-      repl.outputStream.write(out.reverse().join('\n') + '\n');
-      repl.displayPrompt();
-    }
-  };
+    process.on('exit', () => fs.closeSync(fd));
+
+    repl.commands['history'] = {
+        help: 'Show the history',
+        action: function () {
+            let out = [];
+            repl.history.forEach((v, k) => out.push(v));
+            repl.outputStream.write(out.reverse().join('\n') + '\n');
+            repl.displayPrompt();
+        }
+    };
 };
